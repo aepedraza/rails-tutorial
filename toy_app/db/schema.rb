f171c44be839c32a9008d597b15ac424577e31d3
Alvaro Pedraza <aepedraza3117@gmail.com>
2019-08-11 20:20:16 -0300

    toy_app: rails db:migrate

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190811141355) do
+ActiveRecord::Schema.define(version: 20190811231850) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
